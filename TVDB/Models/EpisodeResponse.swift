629bee1d7a4a49b2e3b2158a65d137866c18384b
Pietro Caselani <pc1992@gmail.com>
2017-10-30 16:57:50 -0200

    Remove TVDB targets and use as Pod

--- TVDB/Models/EpisodeResponse.swift
+++
@@ -1,9 +0,0 @@
-import ObjectMapper
-
-public final class EpisodeResponse: ImmutableMappable {
-  public let episode: FullEpisode
-
-  public init(map: Map) throws {
-    self.episode = try map.value("data")
-  }
-}
--- TVDB/Models/FullEpisode.swift
+++
@@ -1,9 +0,0 @@
-import ObjectMapper
-
-public final class FullEpisode: ImmutableMappable {
-  public let filename: String?
-
-  public init(map: Map) throws {
-    self.filename = try? map.value("filename")
-  }
-}
