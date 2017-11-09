48868ba94889136ae2ffc1e25efe7e6647924738
Pietro Caselani <pc1992@gmail.com>
2017-11-09 15:35:16 -0200

    Fix swiftlint warnings

--- CouchTracker/TraktLogin/TraktLoginiOSPresenter.swift
+++ CouchTracker/TraktLogin/TraktLoginiOSPresenter.swift
@@ -18,8 +18,8 @@ final class TraktLoginiOSPresenter: TraktLoginPresenter {
       .subscribe(onSuccess: { [unowned self] url in
         guard let view = self.view else { return }
         view.loadLogin(using: url)
-      }) { [unowned self] error in
-        self.output.logInFail(message: error.localizedDescription)
-      }.disposed(by: disposeBag)
+        }, onError: { [unowned self] error in
+          self.output.logInFail(message: error.localizedDescription)
+      }).disposed(by: disposeBag)
   }
 }
