a663c8354addf9c8c926712487b449898f28eaa1
Pietro Caselani <pc1992@gmail.com>
2017-11-09 15:16:57 -0200

    Fixing some tests

--- CouchTracker/Shows/Progress/ShowsProgressService.swift
+++ CouchTracker/Shows/Progress/ShowsProgressService.swift
@@ -4,16 +4,25 @@ import TraktSwift
 final class ShowsProgressService: ShowsProgressInteractor {
   private let repository: ShowsProgressRepository
   private let showProgressInteractor: ShowProgressInteractor
+  private let scheduler: SchedulerType
 
-  init(repository: ShowsProgressRepository, showProgressInteractor: ShowProgressInteractor) {
+  convenience init(repository: ShowsProgressRepository, showProgressInteractor: ShowProgressInteractor) {
+    let scheduler = ConcurrentDispatchQueueScheduler(queue: DispatchQueue(label: "ShowsProgressServiceQueue"))
+    self.init(repository: repository, showProgressInteractor: showProgressInteractor, scheduler: scheduler)
+  }
+
+  init(repository: ShowsProgressRepository, showProgressInteractor: ShowProgressInteractor, scheduler: SchedulerType) {
     self.repository = repository
     self.showProgressInteractor = showProgressInteractor
+    self.scheduler = scheduler
   }
 
   func fetchWatchedShowsProgress(update: Bool) -> Observable<WatchedShowEntity> {
     return repository.fetchWatchedShows(update: update, extended: .full)
       .flatMap { Observable.from($0) }
       .flatMap { [unowned self] in self.fetchShowProgress(update, $0) }
+      .subscribeOn(scheduler)
+      .observeOn(scheduler)
   }
 
   private func fetchShowProgress(_ update: Bool, _ baseShow: BaseShow) -> Observable<WatchedShowEntity> {
@@ -21,6 +30,8 @@ final class ShowsProgressService: ShowsProgressInteractor {
 
     return showProgressInteractor.fetchShowProgress(update: update, ids: show.ids)
       .flatMap { [unowned self] in return self.mapToEntity(baseShow, builder: $0) }
+      .subscribeOn(scheduler)
+      .observeOn(scheduler)
   }
 
   private func mapToEntity(_ baseShow: BaseShow, builder: WatchedShowBuilder) -> Observable<WatchedShowEntity> {
