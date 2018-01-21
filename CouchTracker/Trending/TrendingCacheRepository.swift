375ff811fba85229a4014c252f62faec0691e296
Pietro Caselani <pc1992@gmail.com>
2018-01-21 17:01:27 -0200

    Refactoring Trending module to deal with movies and show independently

--- CouchTracker/Trending/TrendingCacheRepository.swift
+++ CouchTracker/Trending/TrendingCacheRepository.swift
@@ -3,24 +3,11 @@ import RxSwift
 import TraktSwift
 
 final class TrendingCacheRepository: TrendingRepository {
-//  private let moviesCache: BasicCache<Movies, [TrendingMovie]>
-//  private let showsCache: BasicCache<Shows, [TrendingShow]>
 
 	private let traktProvider: TraktProvider
 
 	init(traktProvider: TraktProvider) {
 		self.traktProvider = traktProvider
-//    let moviesProvider = traktProvider.movies
-//    let showsProvider = traktProvider.shows
-//    self.moviesCache = MemoryCacheLevel<Movies, NSData>()
-//      .compose(DiskCacheLevel<Movies, NSData>())
-//      .compose(MoyaFetcher(provider: moviesProvider))
-//      .transformValues(JSONArrayTransfomer<TrendingMovie>())
-//
-//    self.showsCache = MemoryCacheLevel<Shows, NSData>()
-//      .compose(DiskCacheLevel<Shows, NSData>())
-//      .compose(MoyaFetcher(provider: showsProvider))
-//      .transformValues(JSONArrayTransfomer<TrendingShow>())
 	}
 
 	func fetchMovies(page: Int, limit: Int) -> Observable<[TrendingMovie]> {
--- CouchTracker/Trending/TrendingViewController.swift
+++ CouchTracker/Trending/TrendingViewController.swift
@@ -3,15 +3,12 @@ import RxCocoa
 import RxSwift
 
 final class TrendingViewController: UIViewController {
-  var appConfigurationsPresentable: AppConfigurationsPresentable!
   var presenter: TrendingPresenter!
-  var searchView: SearchView!
 
   private let disposeBag = DisposeBag()
 
   @IBOutlet weak var collectionView: UICollectionView!
   @IBOutlet weak var emptyLabel: UILabel!
-  @IBOutlet weak var searchContainer: UIView!
   @IBOutlet weak var trendingTypeSegmentedControl: UISegmentedControl!
 
   override func awakeFromNib() {
@@ -23,37 +20,16 @@ final class TrendingViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    guard let searchView = searchView as? UIView else {
-      fatalError("searchView should be an instance of UIView")
-    }
-
     guard let collectionViewDataSource = presenter.dataSource as? UICollectionViewDataSource else {
       fatalError("dataSource should be an instance of UICollectionViewDataSource")
     }
 
-    trendingTypeSegmentedControl.setTitle(R.string.localizable.movies(), forSegmentAt: 0)
-    trendingTypeSegmentedControl.setTitle(R.string.localizable.shows(), forSegmentAt: 1)
-
-    trendingTypeSegmentedControl.rx.value
-      .map { $0 == 0 ? TrendingType.movies : TrendingType.shows }
-      .bind(to: presenter.currentTrendingType)
-      .disposed(by: disposeBag)
-
     emptyLabel.text = "No movies to show right now".localized
     collectionView.delegate = self
-
-    searchContainer.addSubview(searchView)
 
     collectionView.dataSource = collectionViewDataSource
 
     presenter.viewDidLoad()
-
-    let settingsItem = UIBarButtonItem(image: R.image.settings(), style: .plain, target: nil, action: nil)
-    settingsItem.rx.tap.subscribe(onNext: { [unowned self] _ in
-      self.appConfigurationsPresentable.showAppSettings()
-    }).disposed(by: disposeBag)
-
-    self.navigationItem.rightBarButtonItem = settingsItem
   }
 }
 
