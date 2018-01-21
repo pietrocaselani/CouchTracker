d1210bfe4855b0f5cfb75a58d4a8c86c97144cdd
Pietro Caselani <pc1992@gmail.com>
2018-01-21 15:53:08 -0200

    Add default tabman configuration for CouchTracker

---
+++ CouchTracker/Extensions/Tabman+CouchTracker.swift
@@ -0,0 +1,15 @@
+import Tabman
+
+extension TabmanBar.Config {
+
+  func defaultCTAppearance() {
+    self.style = .scrollingButtonBar
+    self.appearance = TabmanBar.Appearance({ (appearance) in
+      appearance.style.background = .solid(color: UIColor.ctjaguar)
+      appearance.state.selectedColor = UIColor.white
+      appearance.state.color = UIColor.lightGray
+      appearance.indicator.color = UIColor.ctfog
+    })
+  }
+
+}
