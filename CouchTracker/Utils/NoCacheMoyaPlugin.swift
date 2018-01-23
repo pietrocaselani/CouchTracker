7f6465c54c5324c8d15958d6b5b9e3ef2c55cbac
Pietro Caselani <pc1992@gmail.com>
2018-01-23 01:39:15 -0200

    Add tests for NoCacheMoyaPlugin

--- CouchTracker/Utils/NoCacheMoyaPlugin.swift
+++ CouchTracker/Utils/NoCacheMoyaPlugin.swift
@@ -4,7 +4,7 @@ final class NoCacheMoyaPlugin: PluginType {
   func prepare(_ request: URLRequest, target: TargetType) -> URLRequest {
     var newRequest = request
 
-    newRequest.addValue("no-cache", forHTTPHeaderField: "Cache-Control")
+    newRequest.setValue("no-cache", forHTTPHeaderField: "Cache-Control")    
 
     return newRequest
   }
