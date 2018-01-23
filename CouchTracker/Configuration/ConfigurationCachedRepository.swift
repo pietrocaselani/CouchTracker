3ce21bc028fd7fef2e81684a501e819a4adacd29
Pietro Caselani <pc1992@gmail.com>
2018-01-22 22:25:37 -0200

    Code cleanup; Remove some comments

--- CouchTracker/Configuration/ConfigurationCachedRepository.swift
+++ CouchTracker/Configuration/ConfigurationCachedRepository.swift
@@ -2,16 +2,10 @@ import RxSwift
 import TMDBSwift
 
 final class ConfigurationCachedRepository: ConfigurationRepository {
-
-//  private let cache: BasicCache<ConfigurationService, Configuration>
   private let tmdbProvider: TMDBProvider
 
   init(tmdbProvider: TMDBProvider) {
     self.tmdbProvider = tmdbProvider
-//    self.cache = MemoryCacheLevel<ConfigurationService, NSData>()
-//      .compose(DiskCacheLevel<ConfigurationService, NSData>())
-//      .compose(MoyaFetcher(provider: tmdbProvider.configuration))
-//      .transformValues(JSONObjectTransfomer<Configuration>())
   }
 
   func fetchConfiguration() -> Observable<Configuration> {
--- CouchTracker/Genres/TraktGenreRepository.swift
+++ CouchTracker/Genres/TraktGenreRepository.swift
@@ -3,18 +3,10 @@ import RxSwift
 import TraktSwift
 
 final class TraktGenreRepository: GenreRepository {
-
-//  private let cache: BasicCache<Genres, [Genre]>
   private let traktProvider: TraktProvider
 
   init(traktProvider: TraktProvider) {
     self.traktProvider = traktProvider
-//    let provider = traktProvider.genres
-
-//    self.cache = MemoryCacheLevel<Genres, NSData>()
-//        .compose(DiskCacheLevel<Genres, NSData>())
-//        .compose(MoyaFetcher(provider: provider))
-//        .transformValues(JSONArrayTransfomer<Genre>())
   }
 
   func fetchMoviesGenres() -> Observable<[Genre]> {
