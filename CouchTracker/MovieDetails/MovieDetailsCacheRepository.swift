3c5780defc841d51aa17594f7900ecb0c504bd72
Pietro Caselani <pc1992@gmail.com>
2018-01-22 22:25:37 -0200

    Code cleanup; Remove some comments

--- CouchTracker/MovieDetails/MovieDetailsCacheRepository.swift
+++ CouchTracker/MovieDetails/MovieDetailsCacheRepository.swift
@@ -3,21 +3,13 @@ import RxSwift
 import TraktSwift
 
 final class MovieDetailsCacheRepository: MovieDetailsRepository {
-//  private let cache: BasicCache<Movies, Movie>
   private let traktProvider: TraktProvider
 
   init(traktProvider: TraktProvider) {
-//    let moviesProvider = traktProvider.movies
     self.traktProvider = traktProvider
-
-//    self.cache = MemoryCacheLevel<Movies, NSData>()
-//        .compose(DiskCacheLevel<Movies, NSData>())
-//        .compose(MoyaFetcher(provider: moviesProvider))
-//        .transformValues(JSONObjectTransfomer<Movie>())
   }
 
   func fetchDetails(movieId: String) -> Observable<Movie> {
     return traktProvider.movies.rx.request(.summary(movieId: movieId, extended: .full)).map(Movie.self).asObservable()
-//    return cache.get(.summary(movieId: movieId, extended: .full)).asObservable()
   }
 }
--- CouchTracker/ShowDetails/ShowDetailsCacheRepository.swift
+++ CouchTracker/ShowDetails/ShowDetailsCacheRepository.swift
@@ -3,16 +3,10 @@ import Moya
 import TraktSwift
 
 final class ShowDetailsCacheRepository: ShowDetailsRepository {
-//  private let cache: BasicCache<Shows, Show>
   private let traktProvider: TraktProvider
 
   init(traktProvider: TraktProvider) {
-//    let showsProvider = traktProvider.shows
     self.traktProvider = traktProvider
-
-//    self.cache = MemoryCacheLevel<Shows, NSData>()
-//      .compose(MoyaFetcher(provider: showsProvider))
-//      .transformValues(JSONObjectTransfomer<Show>())
   }
 
   func fetchDetailsOfShow(with identifier: String, extended: Extended) -> Single<Show> {
