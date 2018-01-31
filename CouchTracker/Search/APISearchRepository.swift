1208ee1d0b4d8eaf7c4c5d13082a412bb4898bee
Pietro Caselani <pc1992@gmail.com>
2018-01-31 13:07:24 -0200

    Inject scheduler on APISearchRepository

--- CouchTracker/Search/APISearchRepository.swift
+++ CouchTracker/Search/APISearchRepository.swift
@@ -4,15 +4,15 @@ import TraktSwift
 
 final class APISearchRepository: SearchRepository {
   private let trakt: TraktProvider
+  private let scheduler: SchedulerType
 
-  init(traktProvider: TraktProvider) {
+  init(traktProvider: TraktProvider, scheduler: SchedulerType = ConcurrentDispatchQueueScheduler(qos: .background)) {
     self.trakt = traktProvider
+    self.scheduler = scheduler
   }
 
   func search(query: String, types: [SearchType], page: Int, limit: Int) -> Observable<[SearchResult]> {
     let target = Search.textQuery(types: types, query: query, page: page, limit: limit)
-
-    let scheduler = ConcurrentDispatchQueueScheduler(qos: .background)
 
     return trakt.search.rx.request(target)
       .subscribeOn(scheduler)
