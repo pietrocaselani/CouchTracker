d1783f4376b3073eb8cbb1907b33e684fb848f45
Pietro Caselani <pc1992@gmail.com>
2018-02-09 11:37:11 -0200

    More tests for ShowProgressAPIRepository

--- CouchTrackerTests/ShowProgress/ShowProgressAPIRepositoryTest.swift
+++ CouchTrackerTests/ShowProgress/ShowProgressAPIRepositoryTest.swift
@@ -7,13 +7,8 @@ import TraktSwift
 final class ShowProgressAPIRepositoryTest: XCTestCase {
   private var traktMock: TraktProvider!
 
-  override func setUp() {
-    super.setUp()
-
-    traktMock = TraktProviderMock2()
-  }
-
   func testShowProgressAPIRepository_fetchShowProgress_withoutNextEpisode() {
+    traktMock = TraktProviderMock2(showsName: "trakt_shows_watchedprogress_without_nextepisode")
     let repository = ShowProgressAPIRepository(trakt: traktMock)
 
     let showIds = ShowsProgressMocks.createWatchedShowsMock().first?.show?.ids
@@ -31,7 +26,47 @@ final class ShowProgressAPIRepositoryTest: XCTestCase {
     wait(for: [testExpectation], timeout: 1)
   }
 
+  func testShowProgressAPIRepository_fetchShowProgress_witNextEpisodeError() {
+    traktMock = TraktProviderMock2(episodeName: "trakt_episodes_summary_error")
+    let repository = ShowProgressAPIRepository(trakt: traktMock)
+
+    let showIds = ShowsProgressMocks.createWatchedShowsMock().first?.show?.ids
+
+    let testExpectation = expectation(description: "Should returns a builder without next episode")
+
+    _ = repository.fetchShowProgress(ids: showIds!).subscribe(onSuccess: { builder in
+      testExpectation.fulfill()
+      XCTAssertNotNil(builder.detailShow)
+      XCTAssertNil(builder.episode)
+    }) { error in
+      XCTFail(error.localizedDescription)
+    }
+
+    wait(for: [testExpectation], timeout: 1)
+  }
+
+  func testShowProgressAPIRepository_fetchShowProgress_witNextEpisode() {
+    traktMock = TraktProviderMock2()
+    let repository = ShowProgressAPIRepository(trakt: traktMock)
+
+    let showIds = ShowsProgressMocks.createWatchedShowsMock().first?.show?.ids
+
+    let testExpectation = expectation(description: "Should returns a builder with next episode")
+
+    _ = repository.fetchShowProgress(ids: showIds!).subscribe(onSuccess: { builder in
+      testExpectation.fulfill()
+      XCTAssertNotNil(builder.detailShow)
+      XCTAssertNotNil(builder.episode)
+    }) { error in
+      XCTFail(error.localizedDescription)
+    }
+
+    wait(for: [testExpectation], timeout: 1)
+  }
+
   fileprivate final class TraktProviderMock2: TraktProvider {
+    private let showsName: String?
+    private let episodeName: String?
     var oauth: URL? = nil
 
     var movies: MoyaProvider<Movies> = MoyaProviderMock<Movies>(stubClosure: MoyaProvider.immediatelyStub)
@@ -40,19 +75,30 @@ final class ShowProgressAPIRepositoryTest: XCTestCase {
     var users: MoyaProvider<Users> = MoyaProviderMock<Users>(stubClosure: MoyaProvider.immediatelyStub)
     var authentication: MoyaProvider<Authentication> = MoyaProviderMock<Authentication>(stubClosure: MoyaProvider.immediatelyStub)
     var sync: MoyaProvider<Sync> = MoyaProviderMock<Sync>(stubClosure: MoyaProvider.immediatelyStub)
-    var episodes: MoyaProvider<Episodes> = MoyaProviderMock<Episodes>(stubClosure: MoyaProvider.immediatelyStub)
-    var shows: MoyaProvider<Shows> = MoyaProviderMock<Shows>(endpointClosure: { (shows) -> Endpoint<Shows> in
-      let endpoint = MoyaProvider<Shows>.defaultEndpointMapping(for: shows)
+    lazy var episodes: MoyaProvider<Episodes> = createProviderWith(json: self.episodeName)
+    lazy var shows: MoyaProvider<Shows> = createProviderWith(json: self.showsName)
 
-      let data = TraktEntitiesMock.traktDataForJSON(with: "trakt_shows_watchedprogress_without_nextepisode")
+    init(showsName: String? = nil, episodeName: String? = nil) {
+      self.showsName = showsName
+      self.episodeName = episodeName
+    }
 
-      return Endpoint(url: endpoint.url,
-                                 sampleResponseClosure: { .networkResponse(200, data) },
-                                 method: endpoint.method,
-                                 task: endpoint.task,
-                                 httpHeaderFields: endpoint.httpHeaderFields)
+    private func createProviderWith<T: TraktType>(json name: String?) -> MoyaProviderMock<T> {
+      return MoyaProviderMock<T>(endpointClosure: { (target) -> Endpoint<T> in
+        let endpoint = MoyaProvider<T>.defaultEndpointMapping(for: target)
 
-    }, stubClosure: MoyaProvider.immediatelyStub)
+        guard let jsonName = name else { return endpoint }
+
+        let data = TraktEntitiesMock.traktDataForJSON(with: jsonName)
+
+        return Endpoint(url: endpoint.url,
+                        sampleResponseClosure: { .networkResponse(200, data) },
+                        method: endpoint.method,
+                        task: endpoint.task,
+                        httpHeaderFields: endpoint.httpHeaderFields)
+
+      }, stubClosure: MoyaProvider.immediatelyStub)
+    }
 
     func finishesAuthentication(with request: URLRequest) -> Single<AuthenticationResult> {
       return Single.just(AuthenticationResult.authenticated)
