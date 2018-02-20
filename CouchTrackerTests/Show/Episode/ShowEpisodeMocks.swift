6d87bd44b6dadf9806b9e6600d5e9fb196c31ad2
Pietro Caselani <pc1992@gmail.com>
2018-02-20 18:46:41 -0300

    Add test on ShowEpisodeAPIRepository handling app state

--- CouchTrackerTests/Show/Episode/ShowEpisodeMocks.swift
+++ CouchTrackerTests/Show/Episode/ShowEpisodeMocks.swift
@@ -52,7 +52,11 @@ final class ShowEpisodeMocks {
 	}
 
 	final class ShowEpisodeNetworkMock: ShowEpisodeNetwork {
+		var addToHistoryInvoked = false
+		var removeFromHistoryInvoked = false
+
 		func addToHistory(items: SyncItems) -> Single<SyncResponse> {
+			addToHistoryInvoked = true
 			return Single.deferred { () -> Single<SyncResponse> in
 				let syncResponse: SyncResponse = TraktEntitiesMock.decodeTraktJSON(with: "trakt_sync_addtohistory")
 				return Single.just(syncResponse)
@@ -60,6 +64,7 @@ final class ShowEpisodeMocks {
 		}
 
 		func removeFromHistory(items: SyncItems) -> Single<SyncResponse> {
+			removeFromHistoryInvoked = true
 			return Single.deferred { () -> Single<SyncResponse> in
 				let syncResponse: SyncResponse = TraktEntitiesMock.decodeTraktJSON(with: "trakt_sync_addtohistory")
 				return Single.just(syncResponse)
