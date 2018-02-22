2f811e85060f5046b6899c1fd19f7248e172d30c
Pietro Caselani <pc1992@gmail.com>
2018-02-22 12:14:58 -0300

    Add more tests for ShowsProgressService

--- CouchTrackerCoreTests/Shows/Progress/ShowsProgressServiceTest.swift
+++ CouchTrackerCoreTests/Shows/Progress/ShowsProgressServiceTest.swift
@@ -7,7 +7,7 @@ import TraktSwift
 final class ShowsProgressServiceTest: XCTestCase {
 	private let scheduler = TestSchedulers()
 	private var observer: TestableObserver<[WatchedShowEntity]>!
-	private var repository: ShowsProgressRepository!
+	private var repository: ShowsProgressMocks.ShowsProgressRepositoryMock!
 
 	override func setUp() {
 		super.setUp()
@@ -27,7 +27,39 @@ final class ShowsProgressServiceTest: XCTestCase {
 
 		//Then
 		let entity = ShowsProgressMocks.mockWatchedShowEntity()
-		let expectedEvents = [next(0, [entity]), completed(0)]
+		let expectedEvents = [next(0, [entity])]
+
+		RXAssertEvents(observer.events, expectedEvents)
+	}
+
+	func testShowsProgressService_receiveSameDataFromRepository_emitsOnlyOnce() {
+		//Given
+		let interactor = ShowsProgressService(repository: repository, schedulers: scheduler)
+		_ = interactor.fetchWatchedShowsProgress().subscribe(observer)
+		scheduler.start()
+
+		//When
+		repository.emitsAgain([ShowsProgressMocks.mockWatchedShowEntity()])
+
+		//Then
+		let entity = ShowsProgressMocks.mockWatchedShowEntity()
+		let expectedEvents = [next(0, [entity])]
+
+		RXAssertEvents(observer.events, expectedEvents)
+	}
+
+	func testShowsProgressService_receiveDifferentDataFromRepository_emitsNewData() {
+		//Given
+		let interactor = ShowsProgressService(repository: repository, schedulers: scheduler)
+		_ = interactor.fetchWatchedShowsProgress().subscribe(observer)
+		scheduler.start()
+
+		//When
+		repository.emitsAgain([WatchedShowEntity]())
+
+		//Then
+		let entity = ShowsProgressMocks.mockWatchedShowEntity()
+		let expectedEvents = [next(0, [entity]), next(0, [WatchedShowEntity]())]
 
 		RXAssertEvents(observer.events, expectedEvents)
 	}
