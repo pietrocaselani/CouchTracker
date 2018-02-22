8d2487f3590b88d36bb138bd0d14711a0727b45d
Pietro Caselani <pc1992@gmail.com>
2018-02-22 13:21:01 -0300

    Add tests for ShowProgressCellDefaultPresenter

--- CouchTrackerCore/Shows/ViewModels/WatchedShowViewModel.swift
+++ CouchTrackerCore/Shows/ViewModels/WatchedShowViewModel.swift
@@ -1,9 +1,23 @@
 import Foundation
 
-public struct WatchedShowViewModel {
+public struct WatchedShowViewModel: Hashable {
 	public let title: String
 	public let nextEpisode: String?
 	public let nextEpisodeDate: String?
 	public let status: String
 	public let tmdbId: Int?
+
+	public var hashValue: Int {
+		var hash = title.hashValue ^ status.hashValue
+
+		nextEpisode.run { hash ^= $0.hashValue }
+		nextEpisodeDate.run { hash ^= $0.hashValue }
+		tmdbId.run { hash ^= $0.hashValue }
+
+		return hash
+	}
+
+	public static func == (lhs: WatchedShowViewModel, rhs: WatchedShowViewModel) -> Bool {
+		return lhs.hashValue == rhs.hashValue
+	}
 }
