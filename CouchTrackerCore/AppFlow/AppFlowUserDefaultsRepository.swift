9749d0373318c8102a04a8c4515f6e0bb6a1c8fd
Pietro Caselani <pc1992@gmail.com>
2018-03-07 08:22:04 -0300

    Open app on last selected tab

---
+++ CouchTrackerCore/AppFlow/AppFlowUserDefaultsRepository.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+public final class AppFlowUserDefaultsRepository: AppFlowRepository {
+	private static let lastTabKey = "appFlowLastTab"
+
+	private let userDefaults: UserDefaults
+
+	public var lastSelectedTab: Int {
+		get {
+			return userDefaults.integer(forKey: AppFlowUserDefaultsRepository.lastTabKey)
+		}
+		set {
+			userDefaults.set(newValue, forKey: AppFlowUserDefaultsRepository.lastTabKey)
+		}
+	}
+
+	public init(userDefaults: UserDefaults) {
+		self.userDefaults = userDefaults
+	}
+}
---
+++ CouchTrackerCoreTests/AppFlow/AppFlowInteractorTests.swift
@@ -0,0 +1,20 @@
+import XCTest
+@testable import CouchTrackerCore
+
+final class AppFlowInteractorTests: XCTestCase {
+
+	func testAppFlowInteractor_updateLastSelectedTab_shouldNotifyRepository() {
+		//Given
+		let repository =  AppFlowMocks.Repository()
+		let interactor = AppFlowService(repository: repository)
+
+		//When
+		interactor.lastSelectedTab = 3
+
+		//Then
+		XCTAssertTrue(repository.lastSelectedTabInvoked)
+		XCTAssertEqual(repository.lastSelectedTabParameter, 3)
+		XCTAssertEqual(interactor.lastSelectedTab, 3)
+	}
+
+}
