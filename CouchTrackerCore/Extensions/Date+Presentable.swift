5d6bf7577ec716dbbf7c04753aaa84816fb1883b
Pietro Caselani <pc1992@gmail.com>
2018-05-11 07:27:39 -0300

    Add tests checking the time zone

--- CouchTrackerCore/Extensions/Date+Presentable.swift
+++ CouchTrackerCore/Extensions/Date+Presentable.swift
@@ -5,6 +5,7 @@ extension Date {
 		let dateFormatter = DateFormatter()
 		let format = "short date format".localized
 		dateFormatter.dateFormat = format
+		dateFormatter.timeZone = NSTimeZone.system
 		return dateFormatter.string(from: self)
 	}
 }
