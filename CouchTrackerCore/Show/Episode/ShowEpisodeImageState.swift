362d50894192e6733cf44e38654397679428499d
Pietro Caselani <pc1992@gmail.com>
2018-07-19 08:26:49 -0300

    Create ShowEpisodeViewState struct

---
+++ CouchTrackerCore/Show/Episode/ShowEpisodeImageState.swift
@@ -0,0 +1,9 @@
+//
+//  ShowEpisodeImageState.swift
+//  CouchTracker
+//
+//  Created by Pietro Caselani on 18/07/18.
+//  Copyright © 2018 Pietro Caselani. All rights reserved.
+//
+
+import Foundation
