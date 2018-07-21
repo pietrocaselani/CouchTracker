6ce97d76e32f9cf31319a86e5af47c6a0d21b067
Pietro Caselani <pc1992@gmail.com>
2018-07-21 19:42:03 -0300

    Implementing ViewState on Show Episode module

--- CouchTrackerCore/Show/Episode/ShowEpisodeImageState.swift
+++ CouchTrackerCore/Show/Episode/ShowEpisodeImageState.swift
@@ -1,9 +1,21 @@
-//
-//  ShowEpisodeImageState.swift
-//  CouchTracker
-//
-//  Created by Pietro Caselani on 18/07/18.
-//  Copyright © 2018 Pietro Caselani. All rights reserved.
-//
-
 import Foundation
+
+public enum ShowEpisodeImageState: Hashable {
+	case loading
+	case none
+	case image(url: URL)
+	case error(error: Error)
+
+	public var hashValue: Int {
+		switch self {
+		case .loading: return "ShowEpisodeViewState.loading".hashValue
+		case .none: return "ShowEpisodeViewState.none".hashValue
+		case .image(let url): return "ShowEpisodeViewState.image".hashValue ^ url.hashValue
+		case .error(let error): return "ShowEpisodeViewState.error".hashValue ^ error.localizedDescription.hashValue
+		}
+	}
+
+	public static func == (lhs: ShowEpisodeImageState, rhs: ShowEpisodeImageState) -> Bool {
+		return lhs.hashValue == rhs.hashValue
+	}
+}
--- CouchTrackerCore/Show/Episode/ShowEpisodeViewModel.swift
+++
@@ -1,6 +0,0 @@
-public struct ShowEpisodeViewModel {
-	public let title: String
-	public let number: String
-	public let date: String
-	public let watched: Bool
-}
