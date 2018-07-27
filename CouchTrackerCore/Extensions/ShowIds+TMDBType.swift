f86f9c6bb5ab11731da1724d053492704bc198f0
Pietro Caselani <pc1992@gmail.com>
2018-07-27 06:21:03 -0300

    Add test for GenreRealmEntity

--- CouchTrackerCore/Extensions/ShowIds+TMDBType.swift
+++ CouchTrackerCore/Extensions/ShowIds+TMDBType.swift
@@ -2,7 +2,7 @@ import TraktSwift
 
 extension ShowIds {
 	func tmdbModelType() -> PosterViewModelType? {
-		var type: PosterViewModelType? = nil
+		var type: PosterViewModelType?
 		if let tmdbId = self.tmdb {
 			type = PosterViewModelType.show(tmdbShowId: tmdbId)
 		}
