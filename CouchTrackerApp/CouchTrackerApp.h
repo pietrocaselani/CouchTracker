704b3b1db7b5e50da87cf69b60751cfdcfd18e68
Pietro Caselani <pc1992@gmail.com>
2018-12-23 15:55:13 -0200

    Moving AppConfigurations to CouchTrackerApp

---
+++ CouchTrackerApp/CouchTrackerApp.h
@@ -0,0 +1,19 @@
+//
+//  CouchTrackerApp.h
+//  CouchTrackerApp
+//
+//  Created by Pietro Caselani on 23/12/18.
+//  Copyright © 2018 Pietro Caselani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CouchTrackerApp.
+FOUNDATION_EXPORT double CouchTrackerAppVersionNumber;
+
+//! Project version string for CouchTrackerApp.
+FOUNDATION_EXPORT const unsigned char CouchTrackerAppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CouchTrackerApp/PublicHeader.h>
+
+
