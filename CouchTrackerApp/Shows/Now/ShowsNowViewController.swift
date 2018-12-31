0087751a33365e8e106e57b29a1e143379712e36
Pietro Caselani <pc1992@gmail.com>
2018-12-30 23:48:39 -0200

    Delete ShowsNow storyboard

--- CouchTrackerApp/Shows/Now/ShowsNowViewController.swift
+++ CouchTrackerApp/Shows/Now/ShowsNowViewController.swift
@@ -1,9 +1,19 @@
-
+import Cartography
 
 final class ShowsNowViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
     view.backgroundColor = Colors.View.background
+
+    let label = UILabel()
+    label.text = "Now"
+    label.textColor = Colors.Text.secondaryTextColor
+
+    view.addSubview(label)
+
+    constrain(label) { label in
+      label.center == label.superview!.center
+    }
   }
 }
