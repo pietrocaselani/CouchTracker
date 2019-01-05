9aa1d464c391da1c6927f63149670c1fe34c343e
Pietro Caselani <pc1992@gmail.com>
2019-01-05 21:38:00 -0200

    Sets custom git remote - trying to fix travis PR build

--- ci/travis.sh
+++ ci/travis.sh
@@ -3,7 +3,7 @@
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     echo "Running master.sh"
     ./ci/master.sh
-else
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     echo "Running pull_request.sh"
     ./ci/pull_request.sh
 fi
