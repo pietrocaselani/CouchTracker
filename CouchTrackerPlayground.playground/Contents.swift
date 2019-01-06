c59933633540722331c97acd340303ec5a8ccbc5
Pietro Caselani <pc1992@gmail.com>
2019-01-06 12:08:11 -0200

    Fix bug when episode title was too long and was placed on top of the preview
    Clips to bounds FTW

--- CouchTrackerPlayground.playground/Contents.swift
+++ CouchTrackerPlayground.playground/Contents.swift
@@ -3,7 +3,7 @@
 import UIKit
 import PlaygroundSupport
 
-//import CouchTrackerApp
+import CouchTrackerApp
 import CouchTrackerCore
 import TraktSwift
 import TMDBSwift
@@ -12,17 +12,34 @@ import Kingfisher
 import Cartography
 import RxSwift
 
-enum MyCoolState {
-	case empty
-	case done
-	case error(message: String)
+final class ViewControllerDemo: UIViewController {
+	private var showView: ShowEpisodeView {
+		return self.view as! ShowEpisodeView
+	}
+
+	override func loadView() {
+		view = ShowEpisodeView()
+	}
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		bug1()
+	}
+
+	private func bug1() {
+		let posterLink = "https://image.tmdb.org/t/p/w780/pnUh2RawzYaSU8IjG61MT0AMRyf.jpg"
+		let previewLink = "https://image.tmdb.org/t/p/w500/jFtZF4cdm3yVaL5nNpEFq2hacGl.jpg"
+
+		showView.posterImageView.kf.setImage(with: posterLink.toURL)
+		showView.previewImageView.kf.setImage(with: previewLink.toURL)
+
+		showView.titleLabel.text = "It Was the Worst Day of My Life"
+		showView.overviewLabel.text = "After Annalise chooses Gabriel as her second chair, the unexpected duo puts all of their efforts into Nate Sr.’s murder re-trial as they try to convince a jury to grant an insanity plea. Meanwhile, Bonnie struggles to rebound after a dark part of her past resurfaces."
+
+	}
 }
 
-//let subject = BehaviorSubject<MyCoolState>(value: .empty)
-//
-//func changeSubject() {
-//	subject.onNext(.error(message: "Eita!"))
-//}
-
-let vc = ColorsViewController()
+//let vc = ColorsViewController()
+let vc = ViewControllerDemo()
 PlaygroundPage.current.liveView = vc
