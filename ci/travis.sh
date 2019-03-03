34dfb2bef2715472238aef442b48d2adb295a313
Pietro Caselani <pc1992@gmail.com>
2019-03-03 15:40:15 -0300

    Stop building master branch on Travis

--- ci/travis.sh
+++ ci/travis.sh
@@ -3,7 +3,4 @@
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     echo "Running pull_request.sh"
     ./ci/pull_request.sh
-elif [ "$TRAVIS_BRANCH" = "master" ]; then
-    echo "Running master.sh"
-    ./ci/master.sh
 fi
