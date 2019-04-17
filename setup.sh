2adfe57c6da36d23dd021da3b6366d7ce2c8e7e4
Pietro Caselani <pc1992@gmail.com>
2019-04-17 19:14:35 -0300

    Create Brewfile and uses on setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 function clone_dependency() {
-    echo "Cloning $1"
-    git clone https://github.com/pietrocaselani/$1.git vendor/$1
+    if [ ! -d "vendor/$1" ]; then
+        echo "Cloning $1"
+        git clone "https://github.com/pietrocaselani/${1}.git" "vendor/$1"
+    fi
 }
 
-brew install sonar-scanner
-brew install tailor
-brew install swiftformat
+brew bundle
 sudo pip install lizard
 bundle install --path vendor/bundle
 bundle exec pod repo update
+
 clone_dependency "Trakt-Swift"
 clone_dependency "TMDB-Swift"
 clone_dependency "TVDB-Swift"
