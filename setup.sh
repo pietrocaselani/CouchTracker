9cebf8938e853ef61eaf7726088a659cd661b327
Pietro Caselani <pc1992@gmail.com>
2019-05-18 18:00:50 -0300

    Specify which tuist version to install

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ brew bundle
 sudo pip install lizard
 bundle install --path vendor/bundle
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/tuist/install/master/install)"
+tuist install 0.13.0
