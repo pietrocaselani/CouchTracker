2b9abf88c9e49ba2e316b960aaf8d43a92677f5c
Pietro Caselani <pc1992@gmail.com>
2019-05-18 02:29:08 -0300

    Fix the UI test to handle the alert view controller

--- CouchTrackerUITests/AppStartupEnglishUSTests.swift
+++ CouchTrackerUITests/AppStartupEnglishUSTests.swift
@@ -4,6 +4,9 @@ import KIF
 
 final class AppStartupEnglishUSTests: KIFTestCase {
   func testChangeTabs_enUS() {
+    tester().waitForView(withAccessibilityLabel: "Ok")
+    tester().tapView(withAccessibilityLabel: "Ok")
+
     tester().tapView(withAccessibilityLabel: "Shows")
     tester().tapView(withAccessibilityLabel: "Settings")
     tester().tapView(withAccessibilityLabel: "Movies")
