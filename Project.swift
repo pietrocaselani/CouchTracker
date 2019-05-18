fa4c7f0a5feb744abe3e3436f0187fea3ff8e59b
Pietro Caselani <pc1992@gmail.com>
2019-05-18 12:25:31 -0300

    We are able to generate the project

--- Project.swift
+++ Project.swift
@@ -1,6 +1,140 @@
 import ProjectDescription
 
+// MARK: - Extensions
+
+func + (lhs: [String: String], rhs: [String: String]) -> [String: String] {
+    return lhs.merging(rhs) { first, second in first }
+}
+
+extension Dictionary where Key == String, Value == String {
+    func asConfig() -> Configuration {
+        return Configuration(settings: self)
+    }
+}
+
+
+// MARK: - Constants
+
 let baseBundleId = "io.github.pietrocaselani"
+let miniOSVersion = "10.0"
+
+// MARK: - Target structures
+
+enum CouchTracker {
+    static let name = "CouchTracker"
+    static func target() -> Target {
+        return Target(name: CouchTracker.name,
+                      platform: .iOS,
+                      product: .app,
+                      bundleId: "\(baseBundleId).couchtracker",
+            infoPlist: "CouchTracker/Info.plist",
+            sources: ["CouchTracker/**"],
+            resources: ["CouchTracker/Resources/**"],
+            dependencies: [
+                .target(name: "CouchTrackerApp"),
+                .target(name: "CouchTrackerPersistence"),
+                .target(name: "CouchTrackerDebug"),
+                .target(name: "CouchTrackerCore-iOS"),
+                .target(name: "TMDBSwift-iOS"),
+                .target(name: "TVDBSwift-iOS"),
+                .target(name: "TraktSwift-iOS"),
+                ],
+            settings: settings())
+    }
+
+    private static func settings() -> Settings {
+        let debug = [
+            "PROVISIONING_PROFILE_SPECIFIER": "match Development io.github.pietrocaselani.couchtracker"
+        ] + baseSettingsiOSTargets() + debugCodeSigning()
+
+
+        let release = [
+            "PROVISIONING_PROFILE_SPECIFIER": "match AppStore io.github.pietrocaselani.couchtracker"
+        ] + baseSettingsiOSTargets() + releaseCodeSigning()
+
+        return Settings(debug: debug.asConfig(), release: release.asConfig())
+    }
+}
+
+enum CouchTrackerUITests {
+    static let name = "CouchTrackerUITests"
+
+    static func target() -> Target {
+        return Target(name: CouchTrackerUITests.name,
+                      platform: .macOS,
+                      product: .unitTests,
+                      bundleId: "\(baseBundleId).CouchTrackerUITests",
+            infoPlist: "CouchTrackerUITests/Info.plist",
+            sources: ["CouchTrackerUITests/**"],
+            dependencies: [
+                .target(name: "CouchTrackerAppTestable")
+            ],
+            settings: settings())
+    }
+
+    private static func settings() -> Settings {
+        let base = [
+            "TEST_HOST": "$(BUILT_PRODUCTS_DIR)/CouchTracker.app/CouchTracker",
+            "BUNDLE_LOADER": "$(TEST_HOST)",
+            ] + disableCodeSigning() + baseSettingsiOSTargets()
+        return Settings(base: base)
+    }
+}
+
+enum CouchTrackerApp {
+    static let name = "CouchTrackerApp"
+
+    static func target() -> Target {
+        return Target(name: CouchTrackerApp.name,
+                      platform: .iOS,
+                      product: .framework,
+                      bundleId: "\(baseBundleId).CouchTrackerApp",
+            infoPlist: "CouchTrackerApp/Info.plist",
+            sources: ["CouchTrackerApp/**"],
+            resources: ["CouchTrackerApp/Resources/**"],
+            headers: Headers(public: "CouchTrackerApp/Headers/Public/CouchTrackerApp.h"),
+            dependencies: [
+                .target(name: "CouchTrackerPersistence"),
+                .target(name: "CouchTrackerDebug"),
+                .target(name: "CouchTrackerCore-iOS"),
+                .target(name: "TMDBSwift-iOS"),
+                .target(name: "TVDBSwift-iOS"),
+                .target(name: "TraktSwift-iOS"),
+                ],
+        settings: settings())
+    }
+
+    private static func settings() -> Settings {
+        return Settings(base: baseSettingsiOSTargets(),
+                        debug: debugCodeSigning().asConfig(),
+                        release: releaseCodeSigning().asConfig())
+    }
+}
+
+// MARK: - Settings functions
+
+func baseSettingsiOSTargets() -> [String: String] {
+    return [
+        "IPHONEOS_DEPLOYMENT_TARGET": miniOSVersion,
+        "TARGETED_DEVICE_FAMILY": "1",
+        "CODE_SIGN_STYLE": "Manual",
+        "DEVELOPMENT_TEAM": "B5RPM7SE3L",
+    ]
+}
+
+func disableCodeSigning() -> [String: String] {
+    return ["CODE_SIGN_IDENTITY": ""]
+}
+
+func releaseCodeSigning() -> [String: String] {
+    return ["CODE_SIGN_IDENTITY": "iPhone Distribution"]
+}
+
+func debugCodeSigning() -> [String: String] {
+    return ["CODE_SIGN_IDENTITY": "iPhone Developer"]
+}
+
+// MARK: - Legacy
 
 func traktSwiftTargets() -> [Target] {
   return [
@@ -31,7 +165,7 @@ func traktSwiftTargets() -> [Target] {
            bundleId: "\(baseBundleId).TraktSwiftTests",
            infoPlist: "TraktSwiftTests/Info.plist",
            sources: ["TraktSwiftTests/**"],
-           dependencies: [])
+           dependencies: []),
   ]
 }
 
@@ -64,7 +198,7 @@ func tmdbSwiftTargets() -> [Target] {
            bundleId: "\(baseBundleId).TMDBSwiftTests",
            infoPlist: "TMDBSwiftTests/Info.plist",
            sources: ["TMDBSwiftTests/**"],
-           dependencies: [])
+           dependencies: []),
   ]
 }
 
@@ -97,7 +231,7 @@ func tvdbSwiftTargets() -> [Target] {
            bundleId: "\(baseBundleId).TVDBSwiftTests",
            infoPlist: "TVDBSwiftTests/Info.plist",
            sources: ["TVDBSwiftTests/**"],
-           dependencies: [])
+           dependencies: []),
   ]
 }
 
@@ -131,34 +265,20 @@ func couchTrackerCoreTargets() -> [Target] {
            bundleId: "\(baseBundleId).CouchTrackerCoreTests",
            infoPlist: "CouchTrackerCoreTests/Info.plist",
            sources: ["CouchTrackerCoreTests/**"],
-           dependencies: [])
+           dependencies: []),
   ]
 }
 
 func couchTrackerAppTargets() -> [Target] {
   return [
-    Target(name: "CouchTrackerApp",
-           platform: .iOS,
-           product: .framework,
-           bundleId: "\(baseBundleId).CouchTrackerApp",
-           infoPlist: "CouchTrackerApp/Info.plist",
-           sources: ["CouchTrackerApp/**"],
-           dependencies: [
-             .target(name: "CouchTrackerPersistence"),
-             .target(name: "CouchTrackerDebug"),
-             .target(name: "CouchTrackerCore-iOS"),
-             .target(name: "TMDBSwift-iOS"),
-             .target(name: "TVDBSwift-iOS"),
-             .target(name: "TraktSwift-iOS"),
-           ]),
     Target(name: "CouchTrackerAppTestable",
            platform: .macOS,
            product: .framework,
            bundleId: "\(baseBundleId).CouchTrackerAppTestable",
            infoPlist: "CouchTrackerAppTestable/Info.plist",
            sources: ["CouchTrackerAppTestable/**"],
-           dependencies: [])
-  ]
+           dependencies: []),
+  ] + [CouchTrackerApp.target()]
 }
 
 func persistanceAndDebugTargets() -> [Target] {
@@ -178,44 +298,19 @@ func persistanceAndDebugTargets() -> [Target] {
            sources: ["CouchTrackerDebug/**"],
            dependencies: [
              .target(name: "CouchTrackerCore-iOS"),
-           ])
-  ]
-}
-
-func couchTrackerTargets() -> [Target] {
-  return [
-    Target(name: "CouchTracker",
-           platform: .iOS,
-           product: .app,
-           bundleId: "\(baseBundleId).couchtracker",
-           infoPlist: "CouchTracker/Info.plist",
-           sources: ["CouchTracker/**"],
-           dependencies: [
-             .target(name: "CouchTrackerApp"),
-             .target(name: "CouchTrackerPersistence"),
-             .target(name: "CouchTrackerDebug"),
-             .target(name: "CouchTrackerCore-iOS"),
-             .target(name: "TMDBSwift-iOS"),
-             .target(name: "TVDBSwift-iOS"),
-             .target(name: "TraktSwift-iOS"),
            ]),
-    Target(name: "CouchTrackerUITests",
-           platform: .macOS,
-           product: .unitTests,
-           bundleId: "\(baseBundleId).CouchTrackerUITests",
-           infoPlist: "CouchTrackerUITests/Info.plist",
-           sources: ["CouchTrackerUITests/**"],
-           dependencies: [])
   ]
 }
 
 func allTargets() -> [Target] {
-	let apiTargets = traktSwiftTargets() + tmdbSwiftTargets() + tvdbSwiftTargets()
-	let dependencies = couchTrackerCoreTargets() + couchTrackerAppTargets() + persistanceAndDebugTargets()
-	let appTargets = couchTrackerTargets()
+  let apiTargets = traktSwiftTargets() + tmdbSwiftTargets() + tvdbSwiftTargets()
+  let dependencies = couchTrackerAppTargets() + couchTrackerCoreTargets() + persistanceAndDebugTargets()
+  let appTargets = [CouchTracker.target(), CouchTrackerUITests.target()]
 
-	return apiTargets + dependencies + appTargets
+    return appTargets + dependencies + apiTargets
 }
+
+// MARK: - Project
 
 let project = Project(name: "CouchTracker",
                       targets: allTargets())
