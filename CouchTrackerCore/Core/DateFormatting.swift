8a735c242d8f1f8e04febf92bb1cea08a4251635
Pietro Caselani <pietro.caselani@getyourguide.com>
2019-12-17 14:47:52 +0100

    Fix DateFormattingTests

---
+++ CouchTrackerCore/Core/DateFormatting.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+public enum DateFormatting {
+  public static func shortString(
+    localeProvider: () -> Locale = { DefaultBundleProvider.instance.currentLanguage.asLocale },
+    timeZoneProvider: () -> TimeZone = { NSTimeZone.system },
+    date: Date
+  ) -> String {
+    let dateFormatter = DateFormatter()
+    dateFormatter.locale = localeProvider()
+    dateFormatter.dateFormat = CouchTrackerCoreStrings.shortDateFormat()
+    dateFormatter.timeZone = timeZoneProvider()
+    return dateFormatter.string(from: date)
+  }
+}
+
+extension Date {
+  public func shortString(
+    localeProvider: () -> Locale = { DefaultBundleProvider.instance.currentLanguage.asLocale },
+    timeZoneProvider: () -> TimeZone = { NSTimeZone.system }
+  ) -> String {
+    return DateFormatting.shortString(
+      localeProvider: localeProvider,
+      timeZoneProvider: timeZoneProvider,
+      date: self
+    )
+  }
+}
--- CouchTrackerCore/Localization/SupportedLanguages.swift
+++ CouchTrackerCore/Localization/SupportedLanguages.swift
@@ -4,7 +4,7 @@ public enum SupportedLanguages: String, Hashable, CaseIterable {
   case englishUS = "en_US"
   case portugueseBR = "pt_BR"
 
-  var asLocale: Locale {
+  public var asLocale: Locale {
     return Locale(identifier: rawValue)
   }
 }
