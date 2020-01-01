1de455e924045c5625894f447e4019d33d425c19
Pietro Caselani <pietro.caselani@getyourguide.com>
2020-01-01 04:03:45 +0100

    Add Playground for Rx

---
+++ CouchTrackerPlayground.playground/Pages/Rx.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+import RxSwift
+
+//    let tmp = shows.asObservable().flatMap(Observable.just)
+
+//    Current.trakt.shows.rx.request(.watchedProgress(showId: <#T##String#>, hidden: <#T##Bool#>, specials: <#T##Bool#>, countSpecials: <#T##Bool#>))
+
+func goWords(_ words: Single<[String]>) {
+    let tmp = words.asObservable().flatMap { Observable.from($0) }
+
+    _ = tmp.subscribe(onNext: { print($0) })
+}
+
+goWords(Single.just(["Hello", "PC"]))
