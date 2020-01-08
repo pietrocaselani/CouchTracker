2eda35f146b8b5fa3b39cccc65469e135df8d7f7
Pietro Caselani <pietro.caselani@getyourguide.com>
2020-01-08 20:23:15 +0100

    Move TestableTrakt to TraktSwiftTestable framework

--- TraktSwiftTestable/TestableTrakt.swift
+++ TraktSwiftTestable/TestableTrakt.swift
@@ -1,8 +1,19 @@
-import Moya
 @testable import TraktSwift
+import Moya
 
 public class TestableTrakt: Trakt {
-    override public func createProvider<T>(forTarget target: T.Type) -> MoyaProvider<T> where T: TraktType {
+  public override init(builder: TraktBuilder) {
+    super.init(builder: builder)
+  }
+
+  public convenience init() {
+    self.init(builder: TraktBuilder(buildClosure: {
+      $0.clientId = "fakeClientId"
+      $0.clientSecret = "fakeClientSecret"
+    }))
+  }
+
+  override public func createProvider<T>(forTarget target: T.Type) -> MoyaProvider<T> where T: TraktType {
     let provider = super.createProvider(forTarget: target)
 
     return MoyaProvider(endpointClosure: provider.endpointClosure,
